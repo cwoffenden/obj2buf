16e0964d7006c943c523c1f5dd95ac7b5bba3d24
Carl Woffenden <cwoffenden@gmail.com>
2022-08-04 20:14:10 +0200

    Initial project layout

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/**
+ * \file main.cpp
+ * Wavefront \c .obj to packed buffer.
+ */
+
+#include <cstdlib>
+
+/*
+ * Load and convert.
+ */
+int main(int /*argc*/, char* /*argv*/[]) {
+	return EXIT_SUCCESS;
+}
