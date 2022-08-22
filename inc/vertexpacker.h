16a8fd6a109bbed70827f919c27e05b2e6837a67
Carl Woffenden <cwoffenden@gmail.com>
2022-08-22 19:28:59 +0200

    WIP: decided which bits of Vertex.cpp to reuse

---
+++ inc/vertexpacker.h
@@ -0,0 +1,142 @@
+/**
+ * \file packedvertex.h
+ * Utility to build packed vertex buffers.
+ *
+ * \copyright 2011-2022 Numfum GmbH
+ */
+#pragma once
+
+#include <climits>
+#include <cstddef>
+#include <cstdint>
+
+#include "vec.h"
+
+
+class VertexPacker
+{
+public:
+	/**
+	 * Data storage types.
+	 *
+	 * \note Individual support notes are aimed at ANGLE and older D3D.
+	 *
+	 * \todo re-add support for \c 10_10_10_2 formats?
+	 */
+	enum Storage {
+		/**
+		 * Signed \c byte (normalised to fit the range \c -1.0 to \c +1.0).
+		 *
+		 * \note Incompatible with D3D11 \e Feature \e Level 9_3 vertex data.
+		 */
+		SINT08N,
+		/**
+		 * Signed \c byte (clamped to the range \c -128 to \c +127).
+		 *
+		 * \note Incompatible with D3D11 \e Feature \e Level 9_3 vertex data.
+		 */
+		SINT08C,
+		/**
+		 * Unsigned \c byte (normalised to fit the range \c 0.0 to \c 1.0).
+		 */
+		UINT08N,
+		/**
+		 * Unsigned \c byte (clamped to the range \c 0 to \c 255).
+		 *
+		 * \note Incompatible with D3D index buffers (use \c #UINT16C).
+		 */
+		UINT08C,
+		/**
+		 * Signed \c short (normalised to fit the range \c -1.0 to \c +1.0).
+		 * Stored as two consecutive bytes.
+		 */
+		SINT16N,
+		/**
+		 * Signed \c short (clamped to the range \c -32768 to \c +32767).
+		 * Stored as two consecutive bytes.
+		 */
+		SINT16C,
+		/**
+		 * Unsigned \c short (normalised to fit the range \c 0.0 to \c 1.0).
+		 * Stored as two consecutive bytes.
+		 *
+		 * \note Incompatible with D3D11 \e Feature \e Level 9_3 vertex data.
+		 */
+		UINT16N,
+		/**
+		 * Unsigned \c short (clamped to the range \c 0 to \c 65535). Stored as
+		 * two consecutive bytes.
+		 *
+		 * \note Incompatible with D3D11 \e Feature \e Level 9_3 vertex data.
+		 */
+		UINT16C,
+		/**
+		 * Half-precision \c float (IEEE 754-2008 format). Stored as two
+		 * consecutive bytes.
+		 *
+		 * \note Hardware support should be queried before using.
+		 */
+		FLOAT16,
+		/**
+		 * Single-precision \c float (IEEE 754 format). Stored as four
+		 * consecutive bytes.
+		 */
+		FLOAT32,
+	};
+
+	/**
+	 * Creates a new empty packer.
+	 *
+	 * \param[in] root start of the \e block where the vertex data will be stored
+	 * \param[in] size number of bytes (used only for bounds tests in debug builds)
+	 */
+	VertexPacker(uint8_t* const root, unsigned const size);
+
+#if SIZE_MAX > UINT_MAX
+	/**
+	 * \copydoc VertexPacker(uint8_t*,unsigned)
+	 */
+	inline
+	VertexPacker(uint8_t* const root, size_t const size) : VertexPacker(root, static_cast<unsigned>(size)) {}
+#endif
+
+	/**
+	 * Destroys the packer (noting that the content is owned externally).
+	 */
+	~VertexPacker() = default;
+
+	//****************************************************************************/
+
+	/**
+	 * Adds a number to the data stream, converting to the fit as requested.
+	 *
+	 * \param[in] data value to convert add
+	 * \param[in] type conversion and byte storage
+	 * \return \c true if adding was successful (\c false if no more storage space is available)
+	 */
+	bool add(float const data, Storage const type);
+
+private:
+	/**
+	 * Determines whether \a type can be stored without going out of bounds.
+	 *
+	 * \param[in] type data type to store
+	 * \return \c true if there is sufficient storage space in the underlying buffer
+	 */
+	bool canFit(Storage const type) const;
+
+	/**
+	 * Start of the packed data.
+	 */
+	uint8_t* const root;
+
+	/**
+	 * Next available byte.
+	 */
+	uint8_t* next;
+
+	/**
+	 * Address \e after the last byte of the data allocation.
+	 */
+	uint8_t* const over;
+};
---
+++ src/vertexpacker.cpp
@@ -0,0 +1,68 @@
+#include "vertexpacker.h"
+
+#include <cmath>
+#include <algorithm>
+
+using std::max;
+using std::min;
+
+VertexPacker::VertexPacker(uint8_t* const root, unsigned const size)
+	: root(root)
+	, next(root)
+	, over(root + size) {}
+
+bool VertexPacker::add(float data, Storage const type) {
+	if (canFit(type)) {
+		int32_t temp;
+		switch (type) {
+		case SINT08N:
+			/*
+			 * Note: we're using a pre-GL 4.2 full range normalisation whereby we can
+			 * never express 0.0 (127 = -0.004 and 128 = 0.004).
+			 */
+			data *= ((data < 0.0f) ? 0x0080 : 0x007F);
+			// fall through
+		case SINT08C:
+			temp = min(0x007F, max(static_cast<int32_t>(round(data)), -0x0080));
+			*next++ = temp & 0xFF;
+			break;
+		case UINT08N:
+			data *= 0xFF;
+			// fall through
+		case UINT08C:
+			temp = min(0x00FF, max(static_cast<int32_t>(round(data)),  0x0000));
+			*next++ = temp & 0xFF;
+			break;
+		case SINT16N:
+			data *= ((data < 0.0f) ? 0x8000 : 0x7FFF);
+			// fall through
+		case SINT16C:
+			temp = min(0x7FFF, max(static_cast<int32_t>(round(data)), -0x8000));
+			*next++ = (temp >> 0) & 0xFF;
+			*next++ = (temp >> 8) & 0xFF;
+			break;
+		default:
+			break;
+		}
+		return true;
+	}
+	return false;
+}
+
+bool VertexPacker::canFit(Storage const type) const {
+	switch (type) {
+	case SINT08N:
+	case SINT08C:
+	case UINT08N:
+	case UINT08C:
+		return next + 1 <= over;
+	case SINT16N:
+	case SINT16C:
+	case UINT16N:
+	case UINT16C:
+	case FLOAT16:
+		return next + 2 <= over;
+	default:
+		return next + 4 <= over;
+	}
+}
