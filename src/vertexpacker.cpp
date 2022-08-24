07c19ef6992967765d481a50c361c270f3ad5bcd
Carl Woffenden <cwoffenden@gmail.com>
2022-08-24 15:39:32 +0200

    Initial modern API support

--- src/vertexpacker.cpp
+++ src/vertexpacker.cpp
@@ -39,8 +39,8 @@ static inline int32_t signedLegacy(float const val) {
 
 /**
  * Helper to store a signed float in the range \c -1 to \c 1 as a normalised
- * integer, following the rules for modern OpenGL (desktop 4.2 onwards and ES
- * 3.0 onwards).
+ * integer, following the rules for modern OpenGL (desktop 4.2, ES 3.0 and WebGL
+ * 2.0 onwards) preserving zero.
  *
  * \note See the OpenGL specification for component conversions, e.g. ES 3.0
  * 2.1.6.1 Conversion from Normalized Fixed-Point to Floating-Point.
@@ -50,12 +50,11 @@ static inline int32_t signedLegacy(float const val) {
  * \tparam Bits bit-depth to use (e.g. \c 8 for bytes)
  *
  * \sa https://www.khronos.org/opengl/wiki/Normalized_Integer#Signed
- *
+ */
 template<int Bits>
 static inline int32_t signedModern(float const val) {
-
+	return static_cast<int32_t>(round(val * ((1 << (Bits - 1)) - 1)));
 }
- */
 
 /**
  * Helper to encode a float using the \c VertexPacker#Storage rules for legacy
@@ -92,36 +91,89 @@ static int32_t storeLegacy(float const val, VertexPacker::Storage const type) {
 	}
 }
 
+/**
+ * Helper to encode a float using the \c VertexPacker#Storage rules for modern
+ * OpenGL (desktop 4.2 and ES 3.0 onwards, see \c signedModern()).
+ *
+ * \param[in] val float value to convert
+ * \param[in] type storage type (and rules to follow)
+ */
+static int32_t storeModern(float const val, VertexPacker::Storage const type) {
+	switch (type) {
+	case VertexPacker::SINT08N:
+		return clamp<int32_t>(signedModern<8>(val), -INT8_MAX, INT8_MAX);
+	case VertexPacker::SINT08C:
+		return clamp<int32_t>(int32_t(round(val)), INT8_MIN, INT8_MAX);
+	case VertexPacker::UINT08N:
+		return clamp<int32_t>(int32_t(round(val) * UINT8_MAX), 0, UINT8_MAX);
+	case VertexPacker::UINT08C:
+		return clamp<int32_t>(int32_t(round(val)), 0, UINT8_MAX);
+	case VertexPacker::SINT16N:
+		return clamp<int32_t>(signedModern<16>(val), -INT16_MAX, INT16_MAX);
+	case VertexPacker::SINT16C:
+		return clamp<int32_t>(int32_t(round(val)), INT16_MIN, INT16_MAX);
+	case VertexPacker::UINT16N:
+		return clamp<int32_t>(int32_t(round(val) * UINT16_MAX), 0, UINT16_MAX);
+	case VertexPacker::UINT16C:
+		return clamp<int32_t>(int32_t(round(val)), 0, UINT16_MAX);
+	default: {
+		union {
+			float   f; // where we write
+			int32_t i; // where we read
+		} temp = {val};
+		return temp.i;
+	}
+	}
+}
+
 //*****************************************************************************/
 
-VertexPacker::VertexPacker(void* const root, unsigned const size)
+VertexPacker::VertexPacker(void* const root, unsigned const size, unsigned const opts)
 	: root(static_cast<uint8_t*>(root))
 	, next(static_cast<uint8_t*>(root))
-	, over(static_cast<uint8_t*>(root) + size) {}
+	, over(static_cast<uint8_t*>(root) + size)
+	, opts(opts) {}
 
 bool VertexPacker::add(float const data, Storage const type) {
 	if (hasFreeSpace(type)) {
-		int32_t temp = storeLegacy(data, type);
+		int32_t temp;
+		if ((opts & SIGNED_LEGACY) == 0) {
+			temp = storeModern(data, type);
+		} else {
+			temp = storeLegacy(data, type);
+		}
 		switch (type) {
 		case SINT08N:
 		case SINT08C:
 		case UINT08N:
 		case UINT08C:
-			*next++ = (temp >>  0) & 0xFF;
+			*next++ = temp & 0xFF;
 			break;
 		case SINT16N:
 		case SINT16C:
 		case UINT16N:
 		case UINT16C:
 		case FLOAT16:
-			*next++ = (temp >>  0) & 0xFF;
-			*next++ = (temp >>  8) & 0xFF;
+			if ((opts & BIG_ENDIAN) == 0) {
+				*next++ = (temp >>  0) & 0xFF;
+				*next++ = (temp >>  8) & 0xFF;
+			} else {
+				*next++ = (temp >>  8) & 0xFF;
+				*next++ = (temp >>  0) & 0xFF;
+			}
 			break;
 		default:
-			*next++ = (temp >>  0) & 0xFF;
-			*next++ = (temp >>  8) & 0xFF;
-			*next++ = (temp >> 16) & 0xFF;
-			*next++ = (temp >> 24) & 0xFF;
+			if ((opts & BIG_ENDIAN) == 0) {
+				*next++ = (temp >>  0) & 0xFF;
+				*next++ = (temp >>  8) & 0xFF;
+				*next++ = (temp >> 16) & 0xFF;
+				*next++ = (temp >> 24) & 0xFF;
+			} else {
+				*next++ = (temp >> 24) & 0xFF;
+				*next++ = (temp >> 16) & 0xFF;
+				*next++ = (temp >>  8) & 0xFF;
+				*next++ = (temp >>  0) & 0xFF;
+			}
 		}
 		return true;
 	}
