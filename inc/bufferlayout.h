8af24ba394f86f89ef2ad73d75c5191eb9b56822
Carl Woffenden <cwoffenden@gmail.com>
2022-10-14 18:08:23 +0200

    More or less the final design
    There's still work to do but the bits are all in the right places.

--- inc/bufferlayout.h
+++ inc/bufferlayout.h
@@ -6,6 +6,7 @@
  */
 #pragma once
 
+#include "objvertex.h"
 #include "tooloptions.h"
 
 /**
@@ -15,6 +16,32 @@
 class BufferLayout
 {
 public:
+	/**
+	 * Calculate all of the packing and padding from the user's options.
+	 *
+	 * \param[in] opts command-line options
+	 */
+	BufferLayout(const ToolOptions& opts);
+
+	/**
+	 * Prints the layout to \c stdout (as GL calls).
+	 */
+	void dump() const;
+
+	/**
+	 * Write a single \a vertex to the \a packer using this buffer layout (all
+	 * vertices will be written with the same layout).
+	 *
+	 * \param[in] packer target for the packed vertex
+	 * \param[in] vertex data to write
+	 * \return \c true if adding to \a packer was successful
+	 */
+	bool write(VertexPacker& packer, const ObjVertex& vertex) const;
+
+private:
+	BufferLayout  (const BufferLayout&) = delete; /**< Not copyable   */
+	void operator=(const BufferLayout)  = delete; /**< Not assignable */
+
 	/**
 	 * Packing of the tangent's sign or other components. Where multiple
 	 * components are packed, as would be the case for a second UV channel or
@@ -31,78 +58,110 @@ public:
 	};
 
 	/**
-	 * Calculate all of the packing and padding from the user's options.
-	 *
-	 * \param[in] opts command-line options
-	 */
-	BufferLayout(const ToolOptions& opts);
-
-	/**
-	 * Prints the descriptor to \c stdout (as GL calls, since that or similar
-	 * is what will be used).
-	 *
-	 * \param[in] opts command-line options
-	 */
-	void dump(const ToolOptions& opts) const;
-
-	Packing packSign; /**< Where the single tangent sign was packed. */
-	Packing packTans; /**< Where the encoded tangents pair were packed. */
-
-private:
-	BufferLayout  (const BufferLayout&) = delete; /**< Not copyable   */
-	void operator=(const BufferLayout)  = delete; /**< Not assignable */
-
-	/**
 	 * Parameters associated with a generic interleaved vertex attribute. Later
-	 * these  will be passed, for example, to \c glVertexAttribPointer(), as
+	 * these will be passed, for example, to \c glVertexAttribPointer(), as
 	 * component \c size and \c pointer offset, or in a \c WGPUVertexAttribute
 	 * struct, as the \c format and \c offset (noting that in WebGPU the format
 	 * also encompasses the data type, e.g.: \c WGPUVertexFormat_Float32x2).
 	 */
 	struct AttrParams {
 		/**
-		 * Zero constructor, with the attributes marked as invalid.
+		 * Zero constructor, with the attribute marked as excluded.
 		 */
 		AttrParams();
 
 		/**
-		 * Mark this attribute as valid and set the initial size (the offset
-		 * will be fixed but the size might change as other components are
-		 * packed).
+		 * Mark this attribute as valid and set the initial sizes (the offset
+		 * will be fixed but the number of components might change as other
+		 * components are packed).
 		 *
-		 * \param[in] numComps number of components
+		 * \param[in] attrType storage type
 		 * \param[in] startOff offset to the start of the components
-		 * \param[in] compSize number of bytes in a single component
+		 * \param[in] numComps number of components
 		 */
-		void fill(unsigned const numComps, unsigned const startOff, unsigned const compSize);
+		void fill(VertexPacker::Storage const attrType, unsigned const startOff, unsigned const numComps);
 
-		bool valid;      /**< \c true if these attributes are used and exported. */
-		unsigned size;   /**< Number of components (e.g.: \c 2 for UVs). */
-		unsigned offset; /**< Offset to the first of the components in the interleaved buffer */
-		bool aligned;    /**< \c true if the stream needs 4-byte aligning (and has free space). */
+		/**
+		 * Performs the test for whether this is \c #unaligned (it's nontrivial
+		 * enough to factor into its own method).
+		 */
+		void validate();
+
+		/**
+		 * Calculates the total number of storage bytes required (from the \c
+		 * #storage type and number of \c #components, then 4-byte aligned).
+		 *
+		 * \return the number of bytes required to store a single vertex attribute
+		 */
+		unsigned getAlignedSize() const;
+
+		/**
+		 * Allows testing that this attribute's storage type has been set (and
+		 * isn't the default \c EXCLUDE).
+		 */
+		explicit operator bool() const {
+			return storage;
+		}
+
+		/**
+		 * Prints the attribute to \c stdout (as a GL call).
+		 *
+		 * \param[in] stride bytes between each complete vertex
+		 */
+		void dump(unsigned const stride) const;
+
+		/**
+		 * Storage type for the attribute. The default is \c EXCLUDE, meaning
+		 * this attribute is unused. Once set this should \e not change.
+		 */
+		VertexPacker::Storage storage;
+
+		/**
+		 * Offset to the first of the components in the interleaved buffer.
+		 * Once set this should \e not change (it's accumulated from the
+		 * previous attributes).
+		 */
+		unsigned offset;
+
+		/**
+		 * Number of components (e.g.: \c 2 for UVs). This starts off with an
+		 * initial size but may grow if other attributes are packed in the
+		 * padding (to a maximum of \c 4).
+		 */
+		unsigned components;
+
+		/**
+		 * \c true if the output needs 4-byte aligning. Initially being
+		 * unaligned denotes padding will be written, so the space can be used
+		 * to pack other attributes. This may change as \c #components grows.
+		 */
+		bool unaligned;
 	};
 
 	/**
 	 * Test that \a what still needs packing, that \a attr isn't aligned and
 	 * has enough free space for \a numComps components, and if so, flag it
-	 * then increase the \c AttrParams#size by \c 1.
+	 * then increase the \c AttrParams#components.
 	 * \n
 	 * An example, instead of the convoluted description, would be:
 	 * \code
 	 *	tryPacking(myBuf.packSign, myBuf.posn, 1, PACK_POSN_W)
 	 * \endcode
 	 * Meaning: if \c posn has space for one extra component, then the single
-	 * tangent sign can be packed in the position's \c w axis. Further checks
+	 * bitangent sign can be packed in the position's \c w axis. Further checks
 	 * will have been performed beforehand, verifying the postitions can
 	 * support the required signed type.
 	 *
-	 * \param[in,out] what attribute being packed (e.g. \c #packSign)
+	 * \param[in,out] what item being packed (e.g. \c #packSign)
 	 * \param[in,out] attr which of the attributes to target (e.g.: \c #posn)
 	 * \param[in] numComps number of components required (e.g.: \c 1 for \c #packSign)
-	 * \param[in] where where the sign was packed (for \c posn this would be \c PACK_POSN_W)
+	 * \param[in] where where the item was packed (for \c posn this would be \c PACK_POSN_W)
+	 * \param[in] force \c true if the item should be packed even if \a attr has no padding
 	 */
-	static void tryPacking(Packing& what, AttrParams& attr, int const numComps, Packing const where);
+	static void tryPacking(Packing& what, AttrParams& attr, int const numComps, Packing const where, bool const force = false);
 
+	Packing packSign; /**< Where the single tangent sign was packed. */
+	Packing packTans; /**< Where the encoded tangents pair were packed. */
 	AttrParams posn;  /**< Position attributes. */
 	AttrParams uv_0;  /**< UV channel 0 attributes. */
 	AttrParams norm;  /**< Normal attributes. */
--- src/bufferlayout.cpp
+++ src/bufferlayout.cpp
@@ -7,8 +7,6 @@
 
 #include <cstdio>
 
-#include "tooloptions.h"
-
 BufferLayout::BufferLayout(const ToolOptions& opts)
 	: packSign(PACK_NONE)
 	, packTans(PACK_NONE)
@@ -19,38 +17,37 @@ BufferLayout::BufferLayout(const ToolOptions& opts)
 	 * Starting with all the params at zero, we try to find the best fit.
 	 */
 	unsigned offset = 0;
-	if (unsigned compBytes = opts.posn.bytes()) {
+	if (opts.posn) {
 		/*
-		 * Positions are always X, Y & Z (and the pointer will always be zero).
-		 * For a component size or 1 or 2, the bytes will 3 or 6, needing 1 or
-		 * 2 bytes padding, or (for signed types) allowing the tangent sign to
-		 * be packed.
+		 * Positions are always X, Y & Z (and the offset will always be zero).
+		 * For a storage size or 1 or 2, the total bytes will 3 or 6, needing 1
+		 * or 2 bytes padding, or (for signed types) allowing the bitangent
+		 * sign to be packed.
 		 */
-		posn.fill(3, offset, compBytes);
+		posn.fill(opts.posn, offset, 3);
 		if (hasBitansSign && opts.posn.isSigned()) {
 			tryPacking(packSign, posn, 1, PACK_POSN_W);
 		}
-		offset += ((posn.size * compBytes + 3) / 4) * 4;
-
+		offset += posn.getAlignedSize();
 	}
-	if (unsigned compBytes = opts.text.bytes()) {
+	if (opts.text) {
 		/*
-		 * UVs are always X & Y. A component size of 1 needs 2 bytes, so has
-		 * the extreme of also needing 2 bytes of padding. Shorts (or possibly
-		 * float16s) fit nicely into 4 bytes so are preferred (plus the range is
-		 * more suited). We try to fit the tangent sign, but since signed bytes
-		 * are the only type that will work, it's unlikely to go here.
+		 * UVs are always X & Y. A storage size of 1 needs 2 bytes, so has the
+		 * extreme of also needing 2 bytes of padding. Shorts (or possibly
+		 * float16s) fit nicely into 4 bytes so are preferred (plus the range
+		 * is more suited). We try to fit the bitangent sign, but since signed
+		 * bytes are the only type that will work, it's unlikely to go here.
 		 */
-		uv_0.fill(2, offset, compBytes);
+		uv_0.fill(opts.text, offset, 2);
 		if (hasBitansSign && opts.text.isSigned()) {
 			tryPacking(packSign, uv_0, 1, PACK_UV_0_Z);
 		}
-		offset += ((uv_0.size * compBytes + 3) / 4) * 4;
+		offset += uv_0.getAlignedSize();
 	}
 	// TODO: test that we can pack encoded normals and tangents that are shorts, for example, taking 8 bytes
-	if (unsigned compBytes = opts.norm.bytes()) {
+	if (opts.norm) {
 		bool const encoded = O2B_HAS_OPT(opts.opts, ToolOptions::OPTS_NORMALS_ENCODED);
-		norm.fill((encoded) ? 2 : 3, offset, compBytes);
+		norm.fill(opts.norm, offset, (encoded) ? 2 : 3);
 		if (hasTansPacked && encoded) {
 			tryPacking(packTans, norm, 2, PACK_NORM_Z);
 		} else {
@@ -58,68 +55,133 @@ BufferLayout::BufferLayout(const ToolOptions& opts)
 				tryPacking(packSign, norm, 1, (encoded) ? PACK_NORM_Z : PACK_NORM_W);
 			}
 		}
-		offset += ((norm.size * compBytes + 3) / 4) * 4;
+		offset += norm.getAlignedSize();
 	}
-	if (unsigned compBytes = opts.tans.bytes()) {
+	if (opts.tans) {
 		bool const encoded = O2B_HAS_OPT(opts.opts, ToolOptions::OPTS_NORMALS_ENCODED);
 		if (packTans == PACK_NONE) {
-			tans.fill((encoded) ? 2 : 3, offset, compBytes);
+			tans.fill(opts.tans, offset, (encoded) ? 2 : 3);
 			if (hasBitansSign && opts.tans.isSigned()) {
 				tryPacking(packSign, tans, 1, (encoded) ? PACK_TANS_Z : PACK_TANS_W);
 			}
-			offset += ((tans.size * compBytes + 3) / 4) * 4;
+			offset += tans.getAlignedSize();
 		}
 	}
 	stride = offset;
 }
 
-void BufferLayout::dump(const ToolOptions& opts) const {
-	if (opts.posn) {
-		printf("glVertexAttribPointer(VERT_POSN, %d, GL_%s, GL_%s, %d, %d)\n",
-				posn.size, opts.posn.toString(true), (opts.posn.isNormalized()) ? "TRUE" : "FALSE", stride, posn.offset);
-	}
-	if (opts.text) {
-		printf("glVertexAttribPointer(VERT_UV_0, %d, GL_%s, GL_%s, %d, %d)\n",
-				uv_0.size, opts.text.toString(true), (opts.text.isNormalized()) ? "TRUE" : "FALSE", stride, uv_0.offset);
-	}
-	if (opts.norm) {
-		printf("glVertexAttribPointer(VERT_NORM, %d, GL_%s, GL_%s, %d, %d)\n",
-				norm.size, opts.norm.toString(true), (opts.norm.isNormalized()) ? "TRUE" : "FALSE", stride, norm.offset);
-	}
-	if (opts.tans) {
+void BufferLayout::dump() const {
+	posn.dump(stride);
+	uv_0.dump(stride);
+	norm.dump(stride);
+	if (tans.storage) {
 		if (packTans == PACK_NONE) {
-			printf("glVertexAttribPointer(VERT_TANS, %d, GL_%s, GL_%s, %d, %d)\n",
-				tans.size, opts.tans.toString(true), (opts.tans.isNormalized()) ? "TRUE" : "FALSE", stride, tans.offset);
+			tans.dump(stride);
 		} else {
-			printf("/* Encoded tangents packed in norm.zw (note the four components) */\n");
+			printf("// Encoded tangents packed in norm.zw (note the four components)\n");
 		}
 		if (packSign == PACK_NONE) {
-			printf("glVertexAttribPointer(VERT_BTAN, %d, GL_%s, GL_%s, %d, %d)\n",
-				btan.size, opts.tans.toString(true), (opts.tans.isNormalized()) ? "TRUE" : "FALSE", stride, btan.offset);
+			btan.dump(stride);
 		} else {
-			printf("/* Bitangents sign in posn.w (note the four components) */\n");
+			printf("// Bitangents sign packed in posn.w (note the four components)\n");
 		}
 	}
+}
+
+bool BufferLayout::write(VertexPacker& packer, const ObjVertex& vertex) const {
+	/*
+	 * TODO: encoding normals and tangents
+	 * TODO: general padding
+	 */
+	if (posn) {
+		vertex.posn.store(packer, posn.storage);
+		if (packSign == PACK_POSN_W) {
+			packer.add(vertex.sign, posn.storage);
+		}
+		if (posn.unaligned) {
+			packer.align();
+		}
+	}
+	if (uv_0) {
+		vertex.uv_0.store(packer, uv_0.storage);
+		if (packSign == PACK_UV_0_Z ) {
+			packer.add(vertex.sign, uv_0.storage);
+		}
+		if (uv_0.unaligned) {
+			packer.align();
+		}
+	}
+	if (norm) {
+		vertex.norm.store(packer, norm.storage);
+		if (packTans == PACK_NORM_W) {
+			packer.add(vertex.tans.x, norm.storage);
+			packer.add(vertex.tans.y, norm.storage);
+		}
+		if (norm.unaligned) {
+			packer.align();
+		}
+	}
+	if (tans) {
+		if (packTans == PACK_NONE) {
+			vertex.tans.store(packer, tans.storage);
+			if (tans.unaligned) {
+				packer.align();
+			}
+		}
+	}
+	if (btan) {
+		if (packTans == PACK_NONE) {
+			vertex.btan.store(packer, btan.storage);
+			if (btan.unaligned) {
+				packer.align();
+			}
+		}
+	}
+	return false;
 }
 
 BufferLayout::AttrParams::AttrParams()
-	: valid  (false)
-	, size   (0)
-	, offset (0)
-	, aligned(false) {}
+	: storage   (VertexPacker::Storage::EXCLUDE)
+	, offset    (0)
+	, components(0)
+	, unaligned (false) {}
 
-void BufferLayout::AttrParams::fill(unsigned const numComps, unsigned const startOff, unsigned const compSize) {
-	valid   = true;
-	size    = numComps;
-	offset  = startOff;
-	aligned = ((size * compSize) & 3) == 0;
+void BufferLayout::AttrParams::fill(VertexPacker::Storage const attrType, unsigned const startOff, unsigned const numComps) {
+	storage    = attrType;
+	offset     = startOff;
+	components = numComps;
+	validate();
 }
 
-void BufferLayout::tryPacking(Packing& what, AttrParams& attr, int const numComps, Packing const where) {
+void BufferLayout::AttrParams::validate() {
+	if (storage) {
+		unaligned = ((components * storage.bytes()) & 3) != 0;
+	}
+}
+
+unsigned BufferLayout::AttrParams::getAlignedSize() const {
+	return ((components * storage.bytes() + 3) / 4) * 4;
+}
+
+void BufferLayout::AttrParams::dump(unsigned const stride) const {
+	if (storage) {
+		const char* normalised = (storage.isNormalized()) ? "TRUE" : "FALSE";
+		printf("glVertexAttribPointer(VERT_POSN, %d, GL_%s, GL_%s, %d, %d)\n",
+			components, storage.toString(true), normalised, stride, offset);
+	}
+}
+
+void BufferLayout::tryPacking(Packing& what, AttrParams& attr, int const numComps, Packing const where, bool const force) {
 	if (what == PACK_NONE) {
-		if (attr.aligned == false && (attr.size + numComps) <= 4) {
-			attr.size += numComps;
+		/*
+		 * Simple rules: attr is being used, isn't aligned so needs padding,
+		 * and whether adding extra components will still fit (our limit is GL,
+		 * which supports 1, 2, & 4).
+		 */
+		if (attr && (attr.components + numComps) <= 4 && (attr.unaligned || force)) {
+			attr.components += numComps;
 			what = where;
+			attr.validate();
 		}
 	}
 }
