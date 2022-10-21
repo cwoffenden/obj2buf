6f254da84bbf6ad561d31e177174ce8f94653e64
Carl Woffenden <cwoffenden@gmail.com>
2022-10-21 13:15:31 +0200

    Clang 14 complained about unused vars

--- src/mikktspace.c
+++ src/mikktspace.c
@@ -1202,7 +1202,7 @@ static tbool GenerateTSpaces(STSpace psTspace[], const STriInfo pTriInfos[], con
 	STSpace * pSubGroupTspace = NULL;
 	SSubGroup * pUniSubGroups = NULL;
 	int * pTmpMembers = NULL;
-	int iMaxNrFaces=0, iUniqueTspaces=0, g=0, i=0;
+	int iMaxNrFaces=0, /*iUniqueTspaces=0*,*/ g=0, i=0;
 	for (g=0; g<iNrActiveGroups; g++)
 		if (iMaxNrFaces < pGroups[g].iNrFaces)
 			iMaxNrFaces = pGroups[g].iNrFaces;
@@ -1222,7 +1222,7 @@ static tbool GenerateTSpaces(STSpace psTspace[], const STriInfo pTriInfos[], con
 	}
 
 
-	iUniqueTspaces = 0;
+	//iUniqueTspaces = 0;
 	for (g=0; g<iNrActiveGroups; g++)
 	{
 		const SGroup * pGroup = &pGroups[g];
@@ -1352,7 +1352,7 @@ static tbool GenerateTSpaces(STSpace psTspace[], const STriInfo pTriInfos[], con
 		// clean up and offset iUniqueTspaces
 		for (int s=0; s<iUniqueSubGroups; s++)
 			free(pUniSubGroups[s].pTriMembers);
-		iUniqueTspaces += iUniqueSubGroups;
+		//iUniqueTspaces += iUniqueSubGroups;
 	}
 
 	// clean up
