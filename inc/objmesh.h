b49d68ba083f1c303fb62a1a1a27970529318fc0
Carl Woffenden <cwoffenden@gmail.com>
2023-05-02 17:46:15 +0200

    Refactor all the loading into its own class
    Building has only been tested on Mac and is expected to fail until the includes have been tidied.

---
+++ inc/objmesh.h
@@ -0,0 +1,79 @@
+/**
+ * \file objmesh.h
+ * Wavefront \c .obj loader.
+ *
+ * \copyright 2022 Numfum GmbH
+ */
+
+#include "objvertex.h"
+
+/**
+ * The \c .obj file as vertex and index data. The mesh is loaded into here,
+ * manipulated in-place, then saved out. Once the process starts, if the
+ * original mesh data is needed it will need to be reloaded (or retrieved from a
+ * copy).
+ *
+ * \note This was retrofitted to experimentally support FBX files (but the
+ * naming kept, since \c .obj is what it's modelled on).
+ */
+struct ObjMesh
+{
+public:
+	/**
+	 * Creates a zero-sized mesh (empty buffers, no scale or bias).
+	 */
+	ObjMesh()
+		: scale(1.0f, 1.0f, 1.0f)
+		, bias (0.0f, 0.0f, 0.0f) {};
+
+	/**
+	 * Opens an \c .obj file and extract its content into \a mesh (experimental
+	 * support was added for FBX files, extracting the first mesh found).
+	 *
+	 * \note Any existing content is replaced.
+	 *
+	 * \param[in] srcPath filename of the \c .obj or FBX file
+	 * \param[in] genTans \c true if tangents should be generated
+	 * \param[in] flipG generate tangents for a flipped green channel (by negating the texture's y-axis)
+	 * \return \c true if the file was valid and \a mesh has its content
+	 */
+	bool open(const char* const srcPath, bool const genTans, bool const flipG);
+
+	/**
+	 * Scale the mesh positions so that each is normalised between \c -1 and \c 1.
+	 *
+	 * \param[in] uniform \c true if the same scale should be applied to all axes (otherwise a per-axis scale is applied)
+	 * \param[in] unbiased  \c true if the origin should be maintained at zero (otherwise a bias is applied to make the most of the normalised range)
+	 */
+	void normalise(bool const uniform, bool const unbiased);
+
+	/**
+	 * Run meshopt's various processes (namely vertex cache, overdraw and vertex
+	 * vetch optimisations).
+	 */
+	void optimise();
+
+	/**
+	 * Resizes the buffers (usually as a prelude to filling them).
+	 */
+	void resize(size_t const numVerts, size_t const numIndex);
+
+	//*************************** Public Properties ***************************/
+
+	/**
+	 * Collection of (usually) unique vertices referenced by \c #index.
+	 */
+	ObjVertex::Container verts;
+	/**
+	 * Collection of indices into \c #verts.
+	 */
+	std::vector<unsigned> index;
+	/**
+	 * Scale to apply to each vertex position when drawing (the default is \c 1.0).
+	 */
+	vec3 scale;
+	/**
+	 * Offset to apply to each vertex position when drawing (the default is \c 0.0).
+	 */
+	vec3 bias;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -17,66 +17,15 @@
  * \endcode
  */
 
-#include <cfloat>
 #include <cstdlib>
 #include <cstdio>
 
 #include <chrono>
 
-#include "meshoptimizer.h"
-
 #include "bufferlayout.h"
 #include "fileutils.h"
-#include "objvertex.h"
+#include "objmesh.h"
 #include "tooloptions.h"
-
-/**
- * \def O2B_SMALL_VERT_POS
- * Value that's considered \e small for a vertex position. Values above this can
- * be normalised, below this no processing is done. We choose 1/127, the LSB in
- * a signed 8-bit range (it's just small, and a number had to be picked).
- */
-#ifndef O2B_SMALL_VERT_POS
-#define O2B_SMALL_VERT_POS (1.0f / 127.0f)
-#endif
-
-/**
- * The \c obj file as vertex and index data. The mesh is loaded into here,
- * manipulated in-place, then saved out. Once the process starts, if the
- * original mesh data is needed it will need to be reloaded.
- */
-struct ObjMesh
-{
-	/**
-	 * Creates a zero-sized buffer.
-	 */
-	ObjMesh()
-		: scale(1.0f, 1.0f, 1.0f)
-		, bias (0.0f, 0.0f, 0.0f) {};
-	/**
-	 * Resizes the buffers (usually as a prelude to filling them).
-	 */
-	void resize(size_t const numVerts, size_t const numIndex) {
-		verts.resize(numVerts);
-		index.resize(numIndex);
-	}
-	/**
-	 * Collection of (usually) unique vertices referenced by \c #index.
-	 */
-	ObjVertex::Container verts;
-	/**
-	 * Collection of indices into \c #verts.
-	 */
-	std::vector<unsigned> index;
-	/**
-	 * Scale to apply to each vertex position when drawing (the default is \c 1.0).
-	 */
-	vec3 scale;
-	/**
-	 * Offset to apply to each vertex position when drawing (the default is \c 0.0).
-	 */
-	vec3 bias;
-};
 
 /**
  * Helper to return the current time in milliseconds.
@@ -86,224 +35,6 @@ struct ObjMesh
 static unsigned millis() {
 	return static_cast<unsigned>(std::chrono::duration_cast<std::chrono::milliseconds>(
 						  std::chrono::steady_clock::now().time_since_epoch()).count());
-}
-
-/**
- * Extracts the \c .obj file mesh data as vertex and index buffers.
- *
- * \todo indices should be optional (since they're optional for export) unless we generate then manually export the tris (since we miss out on other meshopt features otherwise)
- *
- * \param[in] obj valid \c fast_obj content
- * \param[in] genTans \c true if tangents should be generated
- * \param[in] flipG generate tangents for a flipped green channel (by negating the texture's y-axis)
- * \param[out] mesh destination for the \c .obj file content
- */
-void extract(fastObjMesh* const obj, bool const genTans, bool const flipG, ObjMesh& mesh) {
-	// No objects or groups, just one big triangle mesh from the file
-	ObjVertex::Container verts;
-	// Content should be in tris but we're going to create fans from any polys
-	unsigned maxVerts = 0;
-	for (unsigned face = 0; face < obj->face_count; face++) {
-		maxVerts += 3 * (obj->face_vertices[face] - 2);
-	}
-	verts.reserve(maxVerts);
-	// Fill the mesh from the expanded raw face data
-	unsigned vertBase = 0;
-	for (unsigned face = 0; face < obj->face_count; face++) {
-		unsigned faceVerts = obj->face_vertices[face];
-		unsigned polyStart = static_cast<unsigned>(verts.size());
-		for (unsigned vert = 0; vert < faceVerts; vert++) {
-			if (vert > 2) {
-				/*
-				 * We create fans for any faces with more than three verts
-				 * (which will only work for convex polys, but this should
-				 * really be processing only tris or quads). We create fans as
-				 * [0, 1, 2], [2, 3, 0], [0, 3, 4], etc., with each added tri
-				 * using the last tri's vert as its starting point, not for vert
-				 * buffer locality but for ease of compression.
-				 */
-				if ((vert & 1) != 0) {
-					verts.push_back(verts[verts.size() - 1]);
-				} else {
-					verts.push_back(verts[polyStart]);
-					verts.push_back(verts[verts.size() - 3]);
-				}
-			}
-			verts.emplace_back(obj, &obj->indices[vertBase + vert]);
-			if (vert > 2) {
-				if ((vert & 1) != 0) {
-					verts.push_back(verts[polyStart]);
-				}
-			}
-		}
-		vertBase += faceVerts;
-	}
-	if (genTans) {
-		ObjVertex::generateTangents(verts, flipG);
-	}
-	// Generate the indices
-	std::vector<unsigned> remap(maxVerts);
-	size_t numVerts = meshopt_generateVertexRemap(remap.data(), NULL, maxVerts, verts.data(), maxVerts, sizeof(ObjVertex));
-	// Now create the buffers we'll be working with (overwriting any existing data)
-	mesh.resize(numVerts, maxVerts);
-	meshopt_remapIndexBuffer (mesh.index.data(), NULL, maxVerts, remap.data());
-	meshopt_remapVertexBuffer(mesh.verts.data(), verts.data(), maxVerts, sizeof(ObjVertex), remap.data());
-}
-
-void extract(ufbx_mesh* const fbx, bool const genTans, bool const flipG, ObjMesh& mesh) {
-	// Same as the fast_obj variant, create one big mesh
-	ObjVertex::Container verts;
-	size_t maxVerts = 0;
-	for (size_t face = 0; face < fbx->num_faces; face++) {
-		maxVerts += 3 * (fbx->faces[face].num_indices - 2);
-	}
-	verts.reserve(maxVerts);
-	for (size_t face = 0; face < fbx->num_faces; face++) {
-		size_t vertBase  = fbx->faces[face].index_begin;
-		size_t faceVerts = fbx->faces[face].num_indices;
-		size_t polyStart = verts.size();
-		for (size_t vert = 0; vert < faceVerts; vert++) {
-			if (vert > 2) {
-				if ((vert & 1) != 0) {
-					verts.push_back(verts[verts.size() - 1]);
-				} else {
-					verts.push_back(verts[polyStart]);
-					verts.push_back(verts[verts.size() - 3]);
-				}
-			}
-			verts.emplace_back(fbx, vertBase + vert);
-			if (vert > 2) {
-				if ((vert & 1) != 0) {
-					verts.push_back(verts[polyStart]);
-				}
-			}
-		}
-	}
-	/*
-	 * It doesn't seem to matter (at least with ufbx importing) whether a Max
-	 * file was exported with Z-up or Y-up, the result is the same (at least
-	 * from the mesh's point of view). Going from Max to Modo, for example, with
-	 * Y-up will add a root transform node, whereas Z-up seems to do the
-	 * transform on import (though it could be tucked away in the pre-transform,
-	 * I didn't look too much). Modo originated content is X-up.
-	 *
-	 * TL;DR: rotate by 90 on the X-axis if Max is the 'original_application' in
-	 * the metadata (it probably needs more experimentation with other apps, but
-	 * it's a simple enough rule for now).
-	 */
-	mat3 rot;
-	rot.set(static_cast<float>(M_PI) / 2, 1.0f, 0.0f, 0.0f);
-	for (ObjVertex::Container::iterator it = verts.begin(); it != verts.end(); ++it) {
-		it->posn = rot.apply(it->posn);
-		it->norm = rot.apply(it->norm);
-	}
-	if (genTans) {
-		ObjVertex::generateTangents(verts, flipG);
-	}
-	std::vector<unsigned> remap(maxVerts);
-	size_t numVerts = meshopt_generateVertexRemap(remap.data(), NULL, maxVerts, verts.data(), maxVerts, sizeof(ObjVertex));
-	// Now create the buffers we'll be working with (overwriting any existing data)
-	mesh.resize(numVerts, maxVerts);
-	meshopt_remapIndexBuffer (mesh.index.data(), NULL, maxVerts, remap.data());
-	meshopt_remapVertexBuffer(mesh.verts.data(), verts.data(), maxVerts, sizeof(ObjVertex), remap.data());
-}
-
-/**
- * Opens an \c .obj file and extract its content into \a mesh.
- *
- * \param[in] srcPath filename of the \c .obj file
- * \param[in] genTans \c true if tangents should be generated
- * \param[in] flipG generate tangents for a flipped green channel (by negating the texture's y-axis)
- * \param[out] mesh destination for the \c .obj file content
- * \return \c true if the file was valid and \a mesh has its content
- */
-bool open(const char* const srcPath, bool const genTans, bool const flipG, ObjMesh& mesh) {
-	bool validSrc = false;
-	if (srcPath) {
-		size_t pathLen = strlen(srcPath);
-		if (pathLen > 4) {
-			if (strncmp(srcPath + (pathLen - 4), ".fbx", 4) == 0) {
-				// Options here:
-				// https://github.com/ufbx/ufbx/blob/70d552625622819e91881ead78c5fe649a855075/ufbx.h#L3853
-				ufbx_load_opts opts = {};
-				opts.ignore_animation   = true;
-				opts.ignore_embedded    = true;
-				opts.skip_skin_vertices = true;
-				ufbx_scene* scene = ufbx_load_file(srcPath, &opts, NULL);
-
-				for (size_t n = 0; n < scene->nodes.count; n++) {
-					ufbx_node* node = scene->nodes.data[n];
-					if (node->mesh) {
-						printf("Mesh %s with %d faces\n", node->name.data, (int) node->mesh->faces.count);
-						extract(node->mesh, genTans, flipG, mesh);
-						validSrc = true;
-						continue;
-					}
-				}
-
-				ufbx_free_scene(scene);
-			}
-		}
-		if (fastObjMesh* obj = fast_obj_read(srcPath)) {
-			/*
-			 * If fast_obj can open a file it will always return a mesh object,
-			 * so we need to perform some minimal validation.
-			 */
-			if (obj->face_count > 0) {
-				extract(obj, genTans, flipG, mesh);
-				validSrc = true;
-			}
-			fast_obj_destroy(obj);
-		}
-	}
-	return validSrc;
-}
-
-/**
- * Scale the mesh positions so that each is normalised between \c -1 and \c 1.
- *
- * \param[in,out] mesh mesh to in-place scale
- * \param[in] uniform \c true if the same scale should be applied to all axes (otherwise a per-axis scale is applied)
- * \param[in] unbiased  \c true if the origin should be maintained at zero (otherwise a bias is applied to make the most of the normalised range)
- */
-void scale(ObjMesh& mesh, bool const uniform, bool const unbiased) {
-	// Get min and max for each component
-	vec3 minPosn({ FLT_MAX,  FLT_MAX,  FLT_MAX});
-	vec3 maxPosn({-FLT_MAX, -FLT_MAX, -FLT_MAX});
-	for (std::vector<ObjVertex>::iterator it = mesh.verts.begin(); it != mesh.verts.end(); ++it) {
-		minPosn = vec3::min(minPosn, it->posn);
-		maxPosn = vec3::max(maxPosn, it->posn);
-	}
-	// Which gives the global mesh scale and offset
-	mesh.scale = (maxPosn - minPosn);
-	if (!unbiased) {
-		mesh.scale = mesh.scale / 2.0f;
-	}
-	// Clamp scale so we don't divide-by-zero on 2D meshes
-	mesh.scale = vec3::max(mesh.scale, vec3(O2B_SMALL_VERT_POS, O2B_SMALL_VERT_POS, O2B_SMALL_VERT_POS));
-	if (uniform) {
-		// Uniform needs to be max(), otherwise the verts could be clamped
-		mesh.scale = std::max(std::max(mesh.scale.x, mesh.scale.y), mesh.scale.z);
-	}
-	// Optionally bias to make the most of the range
-	if (!unbiased) {
-		mesh.bias  = (maxPosn + minPosn) / 2.0f;
-	}
-	// Apply to each vert to normalise
-	for (std::vector<ObjVertex>::iterator it = mesh.verts.begin(); it != mesh.verts.end(); ++it) {
-		it->posn = (it->posn - mesh.bias) / mesh.scale;
-	}
-}
-
-/**
- * Run meshopt's various processes.
- *
- * \param[in,out] mesh mesh to in-place optimise
- */
-void optimise(ObjMesh& mesh) {
-	meshopt_optimizeVertexCache(mesh.index.data(), mesh.index.data(), mesh.index.size(), mesh.verts.size());
-	meshopt_optimizeOverdraw   (mesh.index.data(), mesh.index.data(), mesh.index.size(), mesh.verts[0].posn, mesh.verts.size(), sizeof(ObjVertex), 1.01f);
-	meshopt_optimizeVertexFetch(mesh.verts.data(), mesh.index.data(), mesh.index.size(), mesh.verts.data(),  mesh.verts.size(), sizeof(ObjVertex));
 }
 
 /**
@@ -335,14 +66,14 @@ int main(int argc, const char* argv[]) {
 	unsigned const startMs = millis();
 	bool tans = opts.tans != VertexPacker::Storage::EXCLUDE;
 	bool flip = O2B_HAS_OPT(opts.opts, ToolOptions::OPTS_TANGENTS_FLIP_G);
-	if (!open(srcPath, tans, flip, mesh)) {
+	if (!mesh.open(srcPath, tans, flip)) {
 		fprintf(stderr, "Unable to read: %s\n", (srcPath) ? srcPath : "null");
 		return EXIT_FAILURE;
 	}
 	// Perform an in-place scale/bias if requested
 	if (O2B_HAS_OPT(opts.opts, ToolOptions::OPTS_POSITIONS_SCALE)) {
-		scale(mesh, O2B_HAS_OPT(opts.opts, ToolOptions::OPTS_SCALE_UNIFORM),
-					O2B_HAS_OPT(opts.opts, ToolOptions::OPTS_SCALE_NO_BIAS));
+		mesh.normalise(O2B_HAS_OPT(opts.opts, ToolOptions::OPTS_SCALE_UNIFORM),
+					   O2B_HAS_OPT(opts.opts, ToolOptions::OPTS_SCALE_NO_BIAS));
 	}
 	// In-place normals/tangents/bitangents encode (into the X/Y components)
 	if (O2B_HAS_OPT(opts.opts, ToolOptions::OPTS_NORMALS_ENCODED)) {
@@ -351,7 +82,7 @@ int main(int argc, const char* argv[]) {
 				std::max(opts.norm.fractionBits(), opts.tans.fractionBits()));
 	}
 	// Then the various optimisations
-	optimise(mesh);
+	mesh.optimise();
 	printf("\n");
 	printf("Vertices:  %d\n", static_cast<int>(mesh.verts.size()));
 	printf("Indices:   %d\n", static_cast<int>(mesh.index.size()));
